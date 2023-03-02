965cfea9548619a6ee737857965662ae14b84d7a
Nick <ndavitashvili@aviatrix.com>
2023-03-02 13:00:33 +0000

    initializing X-ray in app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,8 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+
+
 ## Honeycomb
 # OpenTelemetry Libraries for Honeycomb
 from opentelemetry import trace
@@ -31,13 +33,24 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+## X-Ray
+# Import the SDK
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 app = Flask(__name__)
 
-##Honeycomb
+## Honeycomb
 # Initialize automatic instrumentation with Flask
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
+
+## X-Ray
+# Initialize
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
